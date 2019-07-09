dcb8435564a58f3f5f1f1185e73c157a192ce650
yongrhee <user@MZC02-YONGRHEE.local>
2019-07-09 12:54:36 +0900

    callback

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ angular.module('config', [])
   .constant('config',
   {
     prod: {
-      region: 'ap-northeast-1',
-      upload_bucket_name: 'your_bucket_name',
-      identity_pool_id: 'ap-northeast-1:your_pool_id',
+      region: 'ap-northeast-2',
+      upload_bucket_name: 'rekognition-hosting-s3upload-1u48jsat6u0mp',
+      identity_pool_id: 'ap-northeast-2:0ee8fe75-da10-47ac-b9b8-12ab3a8c2055',
       face_collection: 'rekognition-demo-go',
       ddb_table: 'rekognition-demo-go'
 
